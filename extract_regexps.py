423d9d0deb7d8b6b6d3d14b96a9da5d300125d1b
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-05-13 20:01:32 -0500

    Added regexp extractor for Java programs

--- extract_regexps.py
+++ extract_regexps.py
@@ -1,7 +1,9 @@
-from os import listdir
+import glob
+from os import listdir, walk
 from os.path import isfile, join
 import re
 import sys
+
 
 def android_regexp_extraction(path):
     for f in listdir(path):
@@ -14,10 +16,22 @@ def android_regexp_extraction(path):
                 for str in matches:
                     print '"'+str+'"'
 
+
+def java_regexp_extraction(path):
+    for root, subFolders, files in walk(path):
+        for f in files:
+            if f.endswith(".java"):
+                file = open(join(root,f),"r")
+                for line in file.readlines():
+                     matches = {e for e in set(re.findall('Pattern\.compile\("([^"]*)"', line))\
+                               | set(re.findall('matches\("([^"]*)"', line))}
+                     for str in matches:
+                         print '"a'+str+'"'
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         path = sys.argv[1]
     else:
         path = "."
 
-    android_regexp_extraction(path)
+    java_regexp_extraction(path)
