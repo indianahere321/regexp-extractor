cc56f1a2ff6caaa2c0d8a1b9fbea10a7c87379ee
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-05-16 22:17:53 -0500

    Changed android extraction to operate recrusively throughout directories, instead of a fixed directory

--- extract_regexps.py
+++ extract_regexps.py
@@ -6,16 +6,16 @@ import sys
 
 
 def android_regexp_extraction(path):
-    for f in listdir(path):
-        if isfile(join(path,f)):
-            file = open(join(path,f),"r")
-            for line in file.readlines():
-                matches = {e for e in set(re.findall(
-                      'java\.util\.regex\.Pattern compile\(java\.lang\.String\)>\("([^"]*)"', line))\
-                      | set(re.findall('boolean matches\(java\.lang\.String\)>\("([^"]*)"', line))}
-                for str in matches:
-                    print '"'+str+'"'
-
+    for root, subFolders, files in walk(path):
+        for f in files:
+            if f.endswith(".jimple"):
+                file = open(join(root,f),"r")
+                for line in file.readlines():
+                    matches = {e for e in set(re.findall(\
+                               'java\.util\.regex\.Pattern compile\(java\.lang\.String\)>\("([^"]*)"', line))\
+                               | set(re.findall('boolean matches\(java\.lang\.String\)>\("([^"]*)"', line))}
+                    for str in matches:
+                        print '"'+str+'"'
 
 def java_regexp_extraction(path):
     for root, subFolders, files in walk(path):
