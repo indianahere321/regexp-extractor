3430c308e076795b023822011b989eff51775973
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-05-30 15:26:21 -0500

    Changed input flags to either java source or jimple files

--- extract_regexps.py
+++ extract_regexps.py
@@ -9,7 +9,7 @@ regexp_patterns = dict()
 def is_java_file(f):
     return f.endswith(".java")
 
-def is_android_file(f):
+def is_jimple_file(f):
     return f.endswith(".jimple")
 
 def post_process(str):
@@ -27,22 +27,22 @@ def post_process(str):
 
 def initialize_regexp_patterns():
     global regexp_patterns
-    regexp_patterns["-j"] = list()
-    regexp_patterns["-j"].append('Pattern\.compile\("(.*)"')
-    regexp_patterns["-j"].append('matches\("(.*)"')
-    regexp_patterns["-j"] = '|'.join(regexp_patterns["-j"])
+    regexp_patterns["-java"] = list()
+    regexp_patterns["-java"].append('Pattern\.compile\("(.*)"')
+    regexp_patterns["-java"].append('matches\("(.*)"')
+    regexp_patterns["-java"] = '|'.join(regexp_patterns["-java"])
 
-    regexp_patterns["-a"] = list()
-    regexp_patterns["-a"].append('java\.util\.regex\.Pattern compile\(java\.lang\.String\)>\("(.*)"')
-    regexp_patterns["-a"].append('boolean matches\(java\.lang\.String\)>\("(.*)"')
-    regexp_patterns["-a"] = '|'.join(regexp_patterns["-a"])
+    regexp_patterns["-jimple"] = list()
+    regexp_patterns["-jimple"].append('java\.util\.regex\.Pattern compile\(java\.lang\.String\)>\("(.*)"')
+    regexp_patterns["-jimple"].append('boolean matches\(java\.lang\.String\)>\("(.*)"')
+    regexp_patterns["-jimple"] = '|'.join(regexp_patterns["-jimple"])
 
 def regexp_extraction(path, language):
     global regexp_patterns
     regexs = list()
     for root, subFolders, files in walk(path):
         for f in files:
-            if (language == "-a" and is_android_file(f)) or (language == "-j" and is_java_file(f)):
+            if (language == "-jimple" and is_jimple_file(f)) or (language == "-java" and is_java_file(f)):
                 file = open(join(root,f),"r")
                 for line in file.readlines():
                     matches = re.findall(regexp_patterns[language], line)
@@ -68,12 +68,12 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         path = sys.argv[1]
         language = sys.argv[2]
-        if language == "-j" or language == "-a":
+        if language == "-java" or language == "-jimple":
             multiple_regexp_extraction(path, language)
         else:
-            print "Usage: python extract_regexps.py <path> -[a | j]"
+            print "Usage: python extract_regexps.py <path> -[java | jimple]"
     else:
-        print "Usage: python extract_regexps.py <path> -[a | j]"
+        print "Usage: python extract_regexps.py <path> -[java | jimple]"
 
     
 
